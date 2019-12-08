7810adbac5a3c686a329b1ab10907b630a6984a5
crsk <crsk@mail.com>
2019-12-08 14:53:12 -0300

    fixed material module and initialized firebase deploy

--- src/app/shared/modules/material/form-controls.module.ts
+++ src/app/shared/modules/material/form-controls.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepicker } from '@angular/material/datepicker';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatRadioButton } from '@angular/material/radio';
-import { MatSelect } from '@angular/material/select';
-import { MatSlider } from '@angular/material/slider';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 const modules: any[] = [
   MatAutocompleteModule,
   MatCheckboxModule,
-  MatDatepicker,
-  MatFormField,
-  MatInput,
-  MatRadioButton,
-  MatSelect,
-  MatSlider,
-  MatSlideToggle,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
 ]
 
 @NgModule({
