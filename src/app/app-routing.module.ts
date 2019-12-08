26c982abbe5e7fc9ba260ff87ee129ef2136c266
crsk <crsk@mail.com>
2019-12-08 03:40:11 -0300

    updated Angular version to ~8.2.14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '**', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: '🔪', pathMatch: 'full' },
   // loginGuard is used to prevent entering to the login page if the user is already logged in
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [LoginGuard] },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '🔪', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'account', loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule), canActivate: [AuthGuard] },
   { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
+  { path: '**', redirectTo: '🔪' } // redirect to home any wrong path
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
+
 export class AppRoutingModule { }
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,14 +1,14 @@
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor() { }
+  constructor(private router: Router) { }
 
-  canActivate(): Observable<boolean> | boolean {
-    return false
+  canActivate(): Observable<boolean> | boolean | UrlTree {
+    return this.router.createUrlTree(['home'])
   }
 }
