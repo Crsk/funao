f9366dde13268129b6ae533e6c6a7be575fc4987
crsk <crsk@mail.com>
2019-12-08 02:57:47 -0300

    added lazy loading pages structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { LoginGuard } from './shared/guards/login.guard';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  // loginGuard is used to prevent entering to the login page if the user is already logged in
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [LoginGuard] },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'account', loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule), canActivate: [AuthGuard] },
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/admin/admin.module.ts
@@ -0,0 +1,21 @@
+import { AdminComponent } from './admin.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+  }
+]
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+
+export class AdminModule { }
