d356a537d0a204d3aeedbed83fd942b554b1f223
crsk <crsk@mail.com>
2019-12-09 11:24:07 -0300

    fixed create funa dialog module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
+import { CreateFunaModule } from './shared/dialogs/create-funa/create-funa.module';
 import { BasicModule } from './shared/modules/basic/basic.module';
-import { MaterialDataTableModule } from './shared/modules/material/data-table.module';
 import { MaterialLayoutModule } from './shared/modules/material/layout.module';
 import { MaterialNavigationModule } from './shared/modules/material/navigation.module';
 import { MaterialButtonsIndicatorsModule } from './shared/modules/material/buttons-indicators.module';
@@ -9,32 +9,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirebaseModule } from './shared/modules/firebase/firebase.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CreateFunaDialog } from './shared/dialogs/create-funa/create-funa.dialog';
 import { MaterialFormControlsModule } from './shared/modules/material/form-controls.module';
 import { MaterialPopuosModalsModule } from './shared/modules/material/popuos-modals.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateFunaDialog,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FirebaseModule,
-
-    // TODO do something with it...
+    CreateFunaModule,
     BasicModule,
+    
     MaterialNavigationModule,
     MaterialButtonsIndicatorsModule,
     MaterialFormControlsModule,
     MaterialPopuosModalsModule,
     MaterialLayoutModule,
-    MaterialDataTableModule,
   ],
   providers: [],
-  entryComponents: [CreateFunaDialog],
   bootstrap: [AppComponent]
 })
 
--- src/app/shared/dialogs/create-funa/create-funa.module.ts
+++ src/app/shared/dialogs/create-funa/create-funa.module.ts
@@ -1,11 +1,25 @@
 import { CreateFunaDialog } from './create-funa.dialog';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasicModule } from '../../modules/basic/basic.module';
+import { MaterialNavigationModule } from '../../modules/material/navigation.module';
+import { MaterialButtonsIndicatorsModule } from '../../modules/material/buttons-indicators.module';
+import { MaterialFormControlsModule } from '../../modules/material/form-controls.module';
+import { MaterialPopuosModalsModule } from '../../modules/material/popuos-modals.module';
+import { MaterialLayoutModule } from '../../modules/material/layout.module';
+import { MaterialDataTableModule } from '../../modules/material/data-table.module';
 
 @NgModule({
   declarations: [CreateFunaDialog],
-  imports: [CommonModule],
+  imports: [CommonModule,
+    BasicModule,
+    MaterialButtonsIndicatorsModule,
+    MaterialFormControlsModule,
+    MaterialPopuosModalsModule,
+    MaterialLayoutModule,
+  ],
   exports: [CreateFunaDialog],
+  entryComponents: [CreateFunaDialog],
 })
 
 export class CreateFunaModule { }
